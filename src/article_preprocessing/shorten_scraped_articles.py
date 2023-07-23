001337bf12f19df0923ab8a83b6d45ef8b8f4e40
MinaAlmasi <202005465@post.au.dk>
2023-07-23 15:00:26 +0200

    add shorten_scraped_articles

---
+++ src/article_preprocessing/shorten_scraped_articles.py
@@ -0,0 +1,28 @@
+'''
+Script which shortens scraped articles 
+
+Written for the paper titled "Fine-tuning GPT-3 for Synthetic Danish News Generation" (Almasi & Schiønning, 2023).
+'''
+# custom fns 
+from modules.preprocessing_fns import shorten_all_articles, jsonl_article_data
+
+# data wrangling
+import pandas as pd
+
+def main():
+    # define paths 
+    path = pathlib.Path(__file__) 
+    path_data = path.parents[2] / "dummy_data" / "dummy_scraped.csv"
+
+    # read data 
+    data = pd.read_csv(path_data)
+
+    # sample data (randomise)
+    data = data.sample(frac=1)
+
+    # shorten
+    data["short_text"] = shorten_all_articles(data["tekst"])
+
+    # convert into jsonl for gpt-3 finetune 
+    path_out = path.parents[2] / "dummy_jsonl" / "scraped_articles_for_finetune.jsonl"
+    jsonl_article_data(samf_data, "headers", "sub_header", "short_text", path_out)
